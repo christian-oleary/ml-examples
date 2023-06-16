d3db13b6c4373e37a0bea6f51af67b594b1c2f92
Christian O'Leary <christian.oleary@mtu.ie>
2023-06-16 17:45:26 +0100

    update e12 to handle strings in input

--- examples/e12_time_series_features.py
+++ examples/e12_time_series_features.py
@@ -1,9 +1,25 @@
+import numpy as np
 import pandas as pd
+from sklearn.impute import SimpleImputer
+
 from e1_create_dataset import create_regression_dataset
 
-def daily_statistics():
+
+def daily_statistics(input_path=None, output_path='df_daily.csv'):
     # Read the dataset into a DataFrame
-    df, _, __ = create_regression_dataset()
+    if input_path is None:
+        df, _, __ = create_regression_dataset()
+    else:
+        df = pd.read_csv(input_path, index_col=0)
+
+    # Convert strings to NaNs and impute values
+    columns = df.columns
+    index = df.index
+    for col in columns:
+        df[col] = pd.to_numeric(df[col], errors='coerce')
+    df = df.astype(float)
+    df = SimpleImputer(missing_values=np.nan, strategy='mean').fit_transform(df)
+    df = pd.DataFrame(df, index=index, columns=columns)
 
     # Convert the index to datetime format (this should also work for any columns)
     df.index = pd.to_datetime(df.index)
@@ -26,7 +42,8 @@ def daily_statistics():
     df_daily.columns = column_names
 
     # print(df_daily.shape) # It is a good idea to check shapes
-    df_daily.to_csv('df_daily.csv')
+    df_daily.to_csv(output_path)
 
 if __name__ == '__main__':
-    daily_statistics()
+    # daily_statistics()
+    daily_statistics('hourly_data.csv', 'hourly_data_statistics.csv')
